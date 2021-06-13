3fd3ce3311c26e1851b98fa12f6a6624675793b2
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-06-13 15:05:10 +0300

    add new db element

---
+++ Vue.Domain/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vue.Domain
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+        public DateTime Date { get; set; }
+        public int piece { get; set; }
+        public int price { get; set; }
+        public ICollection<User> Users { get; set; }
+    }
+}
