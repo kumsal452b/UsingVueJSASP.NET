4fc3ae258fc078442e2fded16071bb801e7337a9
kumsal452b <yahya.alatas.mdbf17@iste.edu.tr>
2021-06-15 21:38:30 +0300

    add new class element

---
+++ Vue.DAL/Repository/Concrete/ProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Vue.DAL.Repository.Abstract;
+using Vue.Domain;
+
+namespace Vue.DAL.Repository.Concrete
+{
+    public class ProductRepository:Repository<Product>,IProductRepository
+    {
+        public ProductRepository(VueContext context):base(context)
+        {
+
+        }
+    }
+}
